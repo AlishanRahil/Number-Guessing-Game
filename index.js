7ee3a7942ee1f203d91e3aede2c8f86d8eb5a043
Alishan Rahil <160777070+AlishanRahil@users.noreply.github.com>
2024-03-23 03:13:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+import inquirer from "inquirer";
+let random = Math.floor(Math.random() * 6 + 1);
+let answer = await inquirer.prompt([{
+        name: "user",
+        type: "number",
+        message: "Guess The Number Between 1-6"
+    }]);
+if (answer.user === random) {
+    console.log("Conratulations You Gueesd Right");
+}
+else {
+    console.log("You Guessed Wrong");
+}
